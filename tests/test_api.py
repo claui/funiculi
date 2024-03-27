4fbb692918136688688dce61f5e8b55352c85773
Claudia <claui@users.noreply.github.com>
2024-03-27 15:25:33 +0100

    Tests: WIP

---
+++ tests/test_api.py
@@ -0,0 +1,12 @@
+# # pylint: disable=magic-value-comparison, missing-function-docstring, missing-module-docstring, no-self-use, too-many-public-methods
+
+# from funiculi import hello
+
+
+# def test_hello() -> None:
+#     assert hello() == 'Hello, world!'
+
+
+# def test_hello_with_author_name() -> None:
+#     assert hello('Claudia Pellegrino') \
+#         == 'Hello, Claudia Pellegrino!'
